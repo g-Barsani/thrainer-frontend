4cd09f81bfa3ae385392edaa018c887c2bf2a6c3
Gabriel Tavares Barsani <103264805+g-Barsani@users.noreply.github.com>
2025-09-03 15:45:56 -0300

    feat: Improve Login and Register UX. Play video when clicking login button

--- Thrainer/app/src/main/java/com/goblenstudios/thrainer/LoginActivity.kt
+++ Thrainer/app/src/main/java/com/goblenstudios/thrainer/LoginActivity.kt
@@ -43,6 +43,8 @@ class LoginActivity : AppCompatActivity() {
         }
 
         btnLogin.setOnClickListener {
+            playVideo()
+
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
 
@@ -82,5 +84,36 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    fun playVideo() {
+        // Apply the custom theme here
+        val videoDialog = android.app.Dialog(this@LoginActivity, R.style.FullScreenDialogTheme)
+
+        val videoView = android.widget.VideoView(this@LoginActivity)
+        videoView.layoutParams = android.widget.FrameLayout.LayoutParams(
+            android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+            android.view.ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        videoDialog.setContentView(videoView)
+
+        // This line is good to keep, though the theme does most of the work
+        videoDialog.window?.setLayout(
+            android.view.WindowManager.LayoutParams.MATCH_PARENT,
+            android.view.WindowManager.LayoutParams.MATCH_PARENT
+        )
+
+        videoDialog.setCancelable(false)
+        videoView.setVideoPath("android.resource://" + packageName + "/" + R.raw.door_animation)
+
+        videoView.setOnCompletionListener {
+            videoDialog.dismiss()
+            startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
+            overridePendingTransition(0, 0)
+            finish()
+        }
+
+        videoDialog.show()
+        videoView.start()
     }
 }
