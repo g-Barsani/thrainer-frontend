0a8ee1a749473e20d5b56c253d30d8f0a7637793
Gabriel Tavares Barsani <103264805+g-Barsani@users.noreply.github.com>
2025-09-04 12:24:45 -0300

    feat: Remove padding from HomeActivity. Apply text font to buttons

--- Thrainer/app/src/main/java/com/goblenstudios/thrainer/HomeActivity.kt
+++ Thrainer/app/src/main/java/com/goblenstudios/thrainer/HomeActivity.kt
@@ -21,8 +21,6 @@ class HomeActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
